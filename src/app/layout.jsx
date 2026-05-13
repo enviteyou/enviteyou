8b013146a4ed52325b0e286e17bd8452729cb1d3
Raghav Srivastava <raghavsri305@gmail.com>
2026-05-13 17:32:50 +0530

    Add More Sections

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({ children }) {
       <body className="min-h-full overflow-x-hidden bg-white text-black">
         <Header />
         <main className="min-h-screen">{children}</main>
+        <Footer/>
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,13 @@
 import Hero from "../components/Hero";
 import FaqAccordion from "../components/FaqAccordion";
-import Footer from "../components/Footer";
+
 
 export default function Home() {
   return (
     <>
       <Hero />
       <FaqAccordion />
-      <Footer />
+     
     </>
   );
 }
