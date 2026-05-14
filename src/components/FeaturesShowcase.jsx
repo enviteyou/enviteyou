a7d577c1ba36480cf188d71804ee763cbaf52b2f
Raghav Srivastava <raghavsri305@gmail.com>
2026-05-14 14:07:34 +0530

    Add More Sections

--- src/components/FeaturesShowcase.jsx
+++ src/components/FeaturesShowcase.jsx
@@ -83,7 +83,7 @@ export default function FeaturesShowcase() {
             {features.map((feature, index) => (
               <React.Fragment key={feature.label}>
                 <div 
-                  className="sticky top-16 group overflow-hidden transition-all duration-300  py-10 px-0 bg-white"
+                  className="sticky top-16  overflow-hidden transition-all duration-300  py-10 px-0 bg-white"
                   style={{ zIndex: index }}
                 >
                   <div className="flex gap-8 sm:gap-10">
