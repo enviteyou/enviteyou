c1e8be621cad938f996938caa8a601399caa8a63
Raghav Srivastava <raghavsri305@gmail.com>
2026-05-26 13:51:35 +0530

    Add Files

--- src/app/invite/layout.jsx
+++ src/app/invite/layout.jsx
@@ -2,7 +2,7 @@ import "../(site)/globals.css"
 export default function InviteLayout({ children }) {
 	return (
 		<div className="min-h-screen text-black">
-			<div className=" flex min-h-screen w-full  items-start justify-center">
+			<div className=" flex min-h-screen w-full mx-auto max-w-120 items-start justify-center">
 				{children}
 			</div>
 		</div>
